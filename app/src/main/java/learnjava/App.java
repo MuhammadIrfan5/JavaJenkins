f1c77d94b65d430d9ae6a46241c930bad3ed9ac3
Muhammad Irfan <41617528+MuhammadIrfan5@users.noreply.github.com>
2025-07-26 19:49:57 +0100

    Change into Main app

--- app/src/main/java/learnjava/App.java
+++ app/src/main/java/learnjava/App.java
@@ -5,7 +5,7 @@ package learnjava;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! Its Gradle and Jenkins integration.";
     }
 
     public static void main(String[] args) {
